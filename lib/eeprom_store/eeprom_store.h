f5e3ca0248c13d2d2df39d46377ded0c5957279c
girmesh03 <girmachewzewdie03@gmail.com>
2026-03-24 18:41:56 +0300

    phase1: make local libs linkable

---
+++ lib/eeprom_store/eeprom_store.h
@@ -0,0 +1,8 @@
+#ifndef EEPROM_STORE_LIB_HEADER_H
+#define EEPROM_STORE_LIB_HEADER_H
+
+// Forward to the project-level public interface.
+#include "../../include/eeprom_store.h"
+
+#endif
+
---
+++ lib/io_abstraction/io_abstraction.h
@@ -0,0 +1,8 @@
+#ifndef IO_ABSTRACTION_LIB_HEADER_H
+#define IO_ABSTRACTION_LIB_HEADER_H
+
+// Forward to the project-level public interface.
+#include "../../include/io_abstraction.h"
+
+#endif
+
