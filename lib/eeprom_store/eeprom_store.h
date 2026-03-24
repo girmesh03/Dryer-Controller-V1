b7319024a5472472f897eccb10aa1fd7a7493e7b
girmesh03 <girmachewzewdie03@gmail.com>
2026-03-24 18:58:24 +0300

    phase1: remove unused lib wrapper headers

--- lib/eeprom_store/eeprom_store.h
+++
@@ -1,8 +0,0 @@
-#ifndef EEPROM_STORE_LIB_HEADER_H
-#define EEPROM_STORE_LIB_HEADER_H
-
-// Forward to the project-level public interface.
-#include "../../include/eeprom_store.h"
-
-#endif
-
--- lib/io_abstraction/io_abstraction.h
+++
@@ -1,8 +0,0 @@
-#ifndef IO_ABSTRACTION_LIB_HEADER_H
-#define IO_ABSTRACTION_LIB_HEADER_H
-
-// Forward to the project-level public interface.
-#include "../../include/io_abstraction.h"
-
-#endif
-
